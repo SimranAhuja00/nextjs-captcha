9a82a31f87fef20bad33838622fab992581b3dd2
SimranAhuja00 <simranahuja0308@gmail.com>
2023-07-02 23:01:20 +0530

    made changes

--- captcha/app/page.tsx
+++ captcha/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-  <div>
+  <main>
     <input type="text" placeholder="Message"/>
     <div>captcha</div>
     <button>Send</button>
-  </div>
+    </main>
       )
 }
