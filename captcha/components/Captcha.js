2b03203d58a767391379e8fb8756e84ee340f7c6
SimranAhuja00 <simranahuja0308@gmail.com>
2023-07-03 20:08:12 +0530

    updating files

--- captcha/components/Captcha.js
+++ captcha/components/Captcha.js
@@ -1,5 +1,21 @@
 export default function Captcha(){
+
+    const imageLocations = (new Array(9))
+    .fill(null)
+    .map((value, index) => {
+      return `/api/captcha-image/${index}.png`;
+    });
+
     return (
-        <div>captcha goes here</div>
+        <div className="captcha">
+        <h2>Select all dogs: </h2>
+            <div className="captcha-images">
+                {imageLocations.map(imageUrl => (
+                    <div>
+                        <img src={imageUrl} alt=""/>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
